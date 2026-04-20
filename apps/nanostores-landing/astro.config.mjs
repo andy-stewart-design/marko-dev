916d1a76c36bd6d6962f53f26eb0a259949cb6fd
andy-stewart-design <robestewart@ebay.com>
2026-04-20 11:50:32 -0400

    init landing page for nanostores package

---
+++ apps/nanostores-landing/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import marko from "@andystewartdesign/astro-marko";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [marko(), mdx()],
+});
